6c7e7b1641adc1815deaadffc76c3f67c17eae0a
Jalyn.Boyd <Jalyn.Boyd@oneamerica.com>
2022-10-06 00:46:29 -0400

    Created Post top and post image

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,6 +1,21 @@
-function Post({username, img, likeCount, caption, location}) {
+function Post({username, profileImg, likeCount, caption, location}) {
   return (
-    <div>Post</div>
+    <div className="post">
+        <div className="post__top">
+            <div class="post__topLeft">
+                <img src = {profileImg} alt =''/>
+                <p>{username}</p>
+            </div>
+            <div class="post__topRight">
+                {/* more horizon icon */}
+            </div>
+        </div>
+        <div className="post__image">
+            {/* post image */}
+            <img src="https://avatars.githubusercontent.com/u/69220994?v=4"/>
+        </div>
+        <div className='post__bottom'></div> 
+    </div>
   )
 }
 
--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,76 +1,16 @@
 import Post from "./Post"
+import "./css/Posts.css"
 
 function Posts() {
   return (
     <div className="posts">
         {/* Instagram Posts */}
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-
+        <Post profileImg='https://avatars.githubusercontent.com/u/69220994?v=4' username="JayBoyd"/>
+        <Post profileImg='https://avatars.githubusercontent.com/u/69220994?v=4' username="JayBoyd"/>
+        <Post profileImg='https://avatars.githubusercontent.com/u/69220994?v=4' username="JayBoyd"/>
+        <Post profileImg='https://avatars.githubusercontent.com/u/69220994?v=4' username="JayBoyd"/>
+        <Post profileImg='https://avatars.githubusercontent.com/u/69220994?v=4' username="JayBoyd"/>
+        <Post profileImg='https://avatars.githubusercontent.com/u/69220994?v=4' username="JayBoyd"/>
     </div>
   )
 }
