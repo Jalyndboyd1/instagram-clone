09628a405a454044e95105cf133f9182eafb5a3f
Jalyn.Boyd <Jalyn.Boyd@oneamerica.com>
2022-10-06 20:39:58 -0400

    Setup upload input and storage module

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,10 +1,26 @@
+import { useState } from "react"
 import { useSelector } from 'react-redux'
-import { Avatar } from "@mui/material"
+import { Avatar, Button } from "@mui/material"
+import { collection, addDoc } from "firebase/firestore";
+import { db, storage } from "../firebase/app"
 import "./css/Settings.css"
 
 function Settings() {
     const user = useSelector((state) => state.user.user)
+    const [file, setFile] = useState(null)
+    const [caption, setCaption] = useState('')
+    const [progress, setProgress] = useState(0)
 
+    const handleChangeFile = e => {
+        if (e.target.files[0]) setFile(e.target.files[0])
+    }
+
+    const handleUpload = () => {
+        // Uploads Post information
+        // const uploadTask = storage.ref(`images/${file.name}`).put(file)
+    }
+
+    console.log(file)
     return (
         <div className='settings'>
             <div className='settings__info'>
@@ -16,6 +32,11 @@ function Settings() {
             </div>
             <div className='settings__upload'>
                 {/* Upload Images */}
+                <input placeholder='enter caption' value={caption} onChange={(e) => setCaption(e.target.value)} />
+                <input type='file' accept="image/png, image/jpeg" onChange={handleChangeFile} />
+                <Button>
+                    upload
+                </Button>
             </div>
         </div>
     )
--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyACFOblNtEdnO55mmc7uogG_rEEO9rWuNc",
@@ -14,4 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app);
 export const provider = new GoogleAuthProvider()
